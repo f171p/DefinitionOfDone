6e2c4d406d98acfe50b4dabba02ca83fe973adc6
f171p <filip.franek@noseryoung.com>
2022-10-05 16:30:14 +0200

    created code

---
+++ dod.h
@@ -0,0 +1,8 @@
+//
+// Created by filip on 05/10/2022.
+//
+
+#ifndef DOD_H
+#define DOD_H
+void fnaflore();
+#endif //DOD_H
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "dod.h"
+
+int main(){while(1){fnaflore();}}
+
+
